6b72e448e6d397bca7ebb008a0a74125ab144596
Jagruthi <Jagruthi@github.com>
2018-02-04 22:29:51 -0800

    Initial commit

---
+++ TwoSums.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        //create a map and add each element in array with its index as a key
+        Map<Integer,Integer> map = new HashMap(); 
+       for(int i=0 ; i<nums.length; i++ ){
+           map.put(nums[i],i);   
+       }
+        for(int i=0 ; i<nums.length; i++ ){ 
+        
+        if(map.containsKey(target - nums[i]) && map.get(target - nums[i])!= i)
+            return new int[] {i,map.get(target - nums[i])};
+    }
+        throw new IllegalArgumentException("No Solution found");
+ }
+}
